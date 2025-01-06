1110d7e7095b61a99bb3cf3cc3eac969b2a6ed98
Roberto Antonio Navarro Estrada <147531709+Antxnszn@users.noreply.github.com>
2025-01-06 01:51:50 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -83,5 +83,5 @@ def optimize():
         }), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usar el puerto de Azure o 5000 por defecto
-    app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
